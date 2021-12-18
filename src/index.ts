77c59186b3e5a606f1b2df43879cc8dbcd95e696
Chumphot Ngammekchay <n.chumphot@gmail.com>
2021-12-18 16:20:56 +0000

    Added connection to database and GET /restaurants

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { config } from "dotenv";
+import { client } from "./db/db";
 
 const app = express();
 
@@ -8,11 +9,23 @@ app.use(express.json());
 app.use(cors());
 config();
 
-app.listen(process.env.PORT, () => console.log(`Server is listening on port ${process.env.PORT}!`));
+client.connect().then(() => {
+    app.listen(process.env.PORT, () => console.log(`Server is listening on port ${process.env.PORT}!`));
 
 app.get("/", async (req, res) => {
     res.status(200).json({
         status: "success",
         message: "Welcome to Veta's server"
-    })
+    });
+});
+
+app.get("/restaurants", async (req, res) => {
+    const text = "SELECT restaurants.*, addresses.street, addresses.city, addresses.postcode FROM restaurants JOIN addresses ON restaurants.id = addresses.restaurant_id;"
+    const dbres = await client.query(text);
+    res.status(200).json({
+        status: "success",
+        message: "The response body contains all the restaurants.",
+        response: dbres.rows
+    });
+});
 });
